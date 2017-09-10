3a5a699d433048f3b0ed98ed402675293bdc548c
tugorez <tugorez@gmail.com>
2017-09-09 19:13:41 -0500

    added normalization and synonyms to text

--- api/services/ClassificationService.js
+++ api/services/ClassificationService.js
@@ -1,12 +1,14 @@
-service = module.exports = {};
-
+var natural = require('natural');
+var tokenizer = new natural.WordTokenizer();
+var normalize = require('../../normalize.json');
+var synonyms = require('../../synonyms.json');
 var Q = require('q');
 
+service = module.exports = {};
 service.train = train;
 service.test = test;
 service.setAdapter = setAdapter;
 service.classifier = {};
-
 
 function setAdapter(adapter) {
   if (adapter === 'natural') {
@@ -19,43 +21,51 @@ function setAdapter(adapter) {
 function train(links) {
   this.classifier.init();
   links.forEach(function(link) {
-    var doc = link.title + ' ' + link.description + ' ' + link.href;
+    var doc = prepareText(
+      link.title + ' ' + link.description + ' ' + link.href
+    );
     ClassificationService.classifier.learn(doc, link.category);
   });
   this.classifier.train();
 }
 
-
 function test() {
   ClassificationService.setAdapter('bayes');
-  return Link.getTestSets(.1)
-    .then(function(sets) {
-      ClassificationService.train(sets.training);
-      var errors = 0;
-      var results = sets.control.map(runTest);
-      var errors = results.reduce(addErrors, 0);
-      var obj = {
-        errors: errors,
-        totalTested: sets.control.length,
-        totalTrained: sets.training.length,
-        results
-      };
-      //console.log(obj);
-      return obj;
-    });
+  return Link.getTestSets(0.1).then(function(sets) {
+    ClassificationService.train(sets.training);
+    var errors = 0;
+    var results = sets.control.map(runTest);
+    var errors = results.reduce(addErrors, 0);
+    var obj = {
+      errors: errors,
+      totalTested: sets.control.length,
+      totalTrained: sets.training.length,
+      results,
+    };
+    //console.log(obj);
+    return obj;
+  });
 }
 
-function addErrors(sum,value) {
+function addErrors(sum, value) {
   var add = value.expected === value.result ? 0 : 1;
   return sum + add;
-
 }
 
 function runTest(link) {
-  var doc = link.title + ' ' + link.description + ' ' + link.href;
+  var doc = prepareText(link.title + ' ' + link.description + ' ' + link.href);
   var result = ClassificationService.classifier.classify(doc);
   return {
     expected: link.category,
-    result: result
-  }
+    result: result,
+  };
+}
+
+function prepareText(text) {
+  return tokenizer
+    .tokenize(text)
+    .map(token => token.trim().toLowerCase())
+    .map(token => normalize[token] || token)
+    .map(token => synonyms[token] || token)
+    .join(' ');
 }
